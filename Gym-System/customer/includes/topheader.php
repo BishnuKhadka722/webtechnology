ef17245aa6930010bb36e9b7c874a5519bb45f41
BishnuKhadka722 <119380563+BishnuKhadka722@users.noreply.github.com>
2024-12-28 13:31:03 +0545

    Add files via upload
    Further work

---
+++ Gym-System/customer/includes/topheader.php
@@ -0,0 +1,10 @@
+<div id="user-nav" class="navbar navbar-inverse">
+  <ul class="nav right">
+    
+        <li class="divider "></li>
+        <li><a href="../logout.php" ><i class="icon-key"></i> Log Out</a></li>
+      </ul>
+    </>
+    
+   
+</div>
---
+++ Gym-System/customer/pages/index.php
@@ -0,0 +1,52 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Gym System</title>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+<link rel="stylesheet" href="../css/matrix-style.css" />
+<link rel="stylesheet" href="../css/matrix-media.css" />
+<link href="../font-awesome/css/font-awesome.css" rel="stylesheet" />
+
+
+</head>
+<body>
+
+<!--Header-part-->
+<div id="header">
+  <h1><a href="index.php">Perfect Gym System</a></h1>
+</div>
+<!--close-Header-part--> 
+
+
+<!--top-Header-menu-->
+<?php include '../includes/topheader.php'?>
+
+<style>
+#footer {
+  color: white;
+}
+
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  max-width: 460px;
+  margin: auto;
+  text-align: center;
+  font-family: arial;
+}
+
+.title {
+  color: grey;
+  font-size: 18px;
+}
+
+</style>
+
+
+
+
+
+</body>
+</html>
